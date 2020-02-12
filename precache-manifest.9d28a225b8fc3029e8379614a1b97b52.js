c970819be0f89fb68ac518e3ad2b95d3beb63b4c
Eliamar Tani <eliamartani@gmail.com>
2020-02-12 01:20:47 +0000

    Updates

---
+++ precache-manifest.9d28a225b8fc3029e8379614a1b97b52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1055f69b70c5155c94aa1514acd9ba86",
+    "url": "/poco-tic-tac-toe-game/index.html"
+  },
+  {
+    "revision": "9de14f5d01d5c78a1317",
+    "url": "/poco-tic-tac-toe-game/static/css/main.becc935b.chunk.css"
+  },
+  {
+    "revision": "46e5fc4b255ce680aacc",
+    "url": "/poco-tic-tac-toe-game/static/js/2.2c50d84f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/poco-tic-tac-toe-game/static/js/2.2c50d84f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9de14f5d01d5c78a1317",
+    "url": "/poco-tic-tac-toe-game/static/js/main.0a1068e5.chunk.js"
+  },
+  {
+    "revision": "8260baad1c9529375cf3",
+    "url": "/poco-tic-tac-toe-game/static/js/runtime-main.d9e363df.js"
+  }
+]);
